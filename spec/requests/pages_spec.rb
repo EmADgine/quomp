e368758728b3ca98464bfbf57aac9039e5a03e18
EmADgine <matt@emadgine.com>
2014-06-02 15:13:32 -0400

    styling

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,7 +4,13 @@ describe "Pages" do
     describe "Entrance Page" do
         it "should have the content 'Here at Quomp ..." do
             visit '/pages/home'
-            expect(page).to have_content('Here at Quomp...')
+            expect(page).to have_content('Here at Quomp')
+        end
+    end
+    describe "Create PP" do 
+        it "should have the content 'Create Your User Profile'" do
+            visit '/pages/home'
+            expect(page).to have_content('Create Your User Profile')
         end
     end
 end
