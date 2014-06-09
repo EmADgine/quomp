d46ededd41daf44b2f366915084ee1068fc80cd0
EmADgine <matt@emadgine.com>
2014-06-09 10:27:50 -0400

    finished first form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
     get "pages/home"
+    get "pages/cpp"
+    get "pages/ppp"
+    get "pages/ccp"
+    get "pages/pcp"
+    get "pages/jsf"
+    get "pages/trts"
+    get "pages/stp"
+    get "pages/pjap"
+    get "pages/erp"
+    get "pages/spip"
+    get "pages/cp"
+    match '/home', to: 'pages#home', via: 'get'
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe "Pages" do
     describe "Entrance Page" do
-        it "should have the content 'Here at Quomp ..." do
+        it "should have the content 'At Quomp ..." do
             visit '/pages/home'
-            expect(page).to have_content('Here at Quomp')
+            expect(page).to have_content('At Quomp')
         end
     end
-    describe "Create PP" do 
+    describe "Create CP" do 
         it "should have the content 'Create Your User Profile'" do
-            visit '/pages/home'
+            visit '/pages/cpp'
             expect(page).to have_content('Create Your User Profile')
         end
     end
+    describe "Create PP" do 
+        it "should have the content 'Populate Your User Profile'" do
+            visit '/pages/ppp'
+            expect(page).to have_content('Populate Your User Profile')
+        end
+    end
+
 end
