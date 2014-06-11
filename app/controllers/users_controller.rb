0bcabd49c6eb1c87d9d3a00c19c12fbb727c1d65
EmADgine <matt@emadgine.com>
2014-06-11 12:37:12 -0400

    commented out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
           render 'new'
       end
   end
+=begin
   def edit
       @user = User.find(params[:id])
   end
@@ -27,6 +28,7 @@ class UsersController < ApplicationController
           render 'edit'
       end
   end
+=end
   private
     def user_params
         params.require(:user).permit(:name,:email,:password,:password_confirmation)
