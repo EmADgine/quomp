519fc092e84b2ea9d6fcb18d9ff0a307f6cb4928
EmADgine <matt@emadgine.com>
2014-10-06 06:59:25 -0700

    prints

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -203,6 +203,7 @@ module ApplicationHelper
         puts "meta? #{jb.job_meta.nil?}"
         get_idealattributes.each do |attr|
             puts attr.split[0]+" " + eval("jb.job_meta.#{attribute.split[0]}")
+        end
         get_idealattributes.each do |attribute|
             overall+=eval("jb.job_meta.#{attribute.split[0].downcase}")
         end
